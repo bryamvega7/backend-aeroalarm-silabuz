d52004d386ff6649973174cb721f6113a2df657c
StefaniGonzalesL <scgonzalesle@gmail.com>
2023-02-07 03:31:08 -0500

    login backend

---
+++ src/components/midleware/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore} from '@reduxjs/toolkit';
+import { authApi} from '../services/authApi';
+import authReducer from "../features/authSlice";
+import { setupListeners} from "@reduxjs/toolkit/query/react";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    [authApi.reducerPath]: authApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => 
+  getDefaultMiddleware().concat(authApi.middleware),
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+setupListeners(store.dispatch);
